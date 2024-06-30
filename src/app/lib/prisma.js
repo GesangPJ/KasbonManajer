2f2bb56ccbf51964da30e01f335f21f700949664
Gesang Paudra J <g.paudra@gmail.com>
2024-06-30 22:19:14 +0700

    Update prisma.js

--- src/app/lib/prisma.js
+++ src/app/lib/prisma.js
@@ -1,6 +1,9 @@
-// lib/prisma.js
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient();
+const globalForPrisma = global
 
-export default prisma;
+const prisma = globalForPrisma.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
