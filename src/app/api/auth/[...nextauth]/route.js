ddd296242ecd6f117e8f9cef218b4d9d8b0ae2fb
Gesang Paudra J <g.paudra@gmail.com>
2024-07-01 22:19:00 +0700

    fix nextauth

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,65 @@
+// src/app/api/auth/[...nextauth]/route.js
+
+import NextAuth from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import bcrypt from 'bcryptjs'
+
+import prisma from '@/app/lib/prisma'
+
+const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email }
+        })
+
+        if (user && bcrypt.compareSync(credentials.password, user.password)) {
+          return user
+        }
+
+        return null
+      }
+    })
+  ],
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: 'jwt',
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_SECRET,
+  },
+  pages: {
+    signIn: '/', // Sign-in page route
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id
+        token.email = user.email
+        token.name = user.name
+        token.userType = user.userType
+      }
+
+      return token
+    },
+    async session({ session, token }) {
+      session.user.id = token.id
+      session.user.email = token.email
+      session.user.name = token.name
+      session.user.userType = token.userType
+
+      return session
+    }
+  }
+}
+
+export const GET = NextAuth(authOptions)
+export const POST = NextAuth(authOptions)
