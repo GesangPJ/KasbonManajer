05bbbde995e9f7b1d2636dfdca183bda029d2e4d
Gesang Paudra J <g.paudra@gmail.com>
2024-07-02 17:08:14 +0700

    update root Layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 // Third-party Imports
 import 'react-perfect-scrollbar/dist/css/styles.css';
 
@@ -9,19 +11,21 @@ import '@assets/iconify-icons/generated-icons.css';
 
 import { SessionProvider } from 'next-auth/react';
 
-export const metadata = {
-  title: 'Kasbon Manager',
-  description:
-    'Aplikasi manajer Kasbon dengan NextJS 14 Full Stack',
-};
+// export const metadata = {
+//   title: 'Kasbon Manager',
+//   description: 'Aplikasi Pengelola Kasbon dengan NextJS 14 Full Stack',
+// };
 
 const RootLayout = ({ children }) => {
   const direction = 'ltr';
 
   return (
     <html id='__next' dir={direction}>
+      <head>
+        <title>Kasbon Manager</title>
+      </head>
       <body className='flex is-full min-bs-full flex-auto flex-col'>
-        <SessionProvider session={pageProps.session}>
+        <SessionProvider>
           {children}
         </SessionProvider>
       </body>
