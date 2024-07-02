827b79e2c1a8247b9e57eba7a3afc43c8a15029b
Gesang Paudra J <g.paudra@gmail.com>
2024-07-02 21:14:41 +0700

    working API tambah kasbon

--- src/app/api/tambah-kasbon/route.js
+++ src/app/api/tambah-kasbon/route.js
@@ -6,7 +6,7 @@ import prisma from '@/app/lib/prisma'
 
 export const POST = async (req) => {
   try {
-    const { userId, jumlah, keterangan } = await req.json()
+    const { userId, jumlah, keterangan, metode } = await req.json()
 
     if (!userId || !jumlah || !keterangan || !metode) {
       return NextResponse.json({ error: "Semua bidang harus diisi." }, { status: 400 });
@@ -15,8 +15,8 @@ export const POST = async (req) => {
     // Ambil tanggal dan waktu saat ini
     const now = new Date()
     const createdAt = now.toISOString() // menggunakan format ISO untuk datetime
-    const status_r = 'BELUM'
-    const status_b = 'BELUM'
+    const status_r = "BELUM"
+    const status_b = "BELUM"
 
     try {
       const kasbon = await prisma.kasbon.create({
@@ -26,17 +26,24 @@ export const POST = async (req) => {
           status_r,
           status_b,
           keterangan,
-          metode: metode.toUpperCase(),
+          metode,
           createdAt,
-          updatedAt: createdAt, // updatedAt diatur dengan nilai yang sama dengan createdAt
+
+          // updatedAt: createdAt
         },
       })
 
+      console.log('Kasbon created:', kasbon)
+
       return NextResponse.json(kasbon, { status: 201 })
     } catch (error) {
+      console.error('Error membuat kasbon:', error)
+
       return NextResponse.json({ error: "Kasbon sudah ada" }, { status: 400 })
     }
   } catch (error) {
+    console.error('Error membuat kasbon:', error)
+
     return NextResponse.json({ error: "Terjadi kesalahan saat memproses permintaan." }, { status: 500 })
   }
 }
