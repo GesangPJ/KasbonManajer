806fb23a90a44901c1f1a1e738874ac9d24b3767
Gesang Paudra J <g.paudra@gmail.com>
2024-07-02 20:18:38 +0700

    update tambah kasbon api

--- src/app/api/tambah-kasbon/route.js
+++ src/app/api/tambah-kasbon/route.js
@@ -1,3 +1,5 @@
+// Tambah Kasbon API. Lokasi : /src/app/api/tambah-kasbon/route.js
+
 import { NextResponse } from 'next/server'
 
 import prisma from '@/app/lib/prisma'
@@ -6,23 +8,31 @@ export const POST = async (req) => {
   try {
     const { userId, jumlah, keterangan } = await req.json()
 
-    const createdAt = ''
+    // Ambil tanggal dan waktu saat ini
+    const now = new Date()
+    const createdAt = now.toISOString() // menggunakan format ISO untuk datetime
+    const status_r = 'BELUM'
+    const status_b = 'BELUM'
 
     try {
       const kasbon = await prisma.kasbon.create({
         data: {
           userId,
           jumlah,
+          status_r,
+          status_b,
           keterangan,
           metode: metode.toUpperCase(),
-          createdAt,},
-      });
+          createdAt,
+          updatedAt: createdAt, // updatedAt diatur dengan nilai yang sama dengan createdAt
+        },
+      })
 
-      return NextResponse.json(kasbon, { status: 201 });
+      return NextResponse.json(kasbon, { status: 201 })
     } catch (error) {
-      return NextResponse.json({ error: "Kasbon sudah ada" }, { status: 400 });
+      return NextResponse.json({ error: "Kasbon sudah ada" }, { status: 400 })
     }
   } catch (error) {
-    return NextResponse.json({ error: "Terjadi kesalahan saat memproses permintaan." }, { status: 500 });
+    return NextResponse.json({ error: "Terjadi kesalahan saat memproses permintaan." }, { status: 500 })
   }
 }
