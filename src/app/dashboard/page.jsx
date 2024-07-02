d219bd4961b1552e2c6078d213934ce1a4d531cf
Gesang Paudra J <g.paudra@gmail.com>
2024-07-02 17:06:56 +0700

    test get session data in dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,24 +1,24 @@
-// MUI Imports
+'use client'
+
+import { useSession } from 'next-auth/react'
+
 import Grid from '@mui/material/Grid'
 
-// Components Imports
-import Award from '@views/dashboard/Award'
-import Transactions from '@views/dashboard/Transactions'
-import WeeklyOverview from '@views/dashboard/WeeklyOverview'
-import TotalEarning from '@views/dashboard/TotalEarning'
-import LineChart from '@views/dashboard/LineChart'
-import DistributedColumnChart from '@views/dashboard/DistributedColumnChart'
-import DepositWithdraw from '@views/dashboard/DepositWithdraw'
-import SalesByCountries from '@views/dashboard/SalesByCountries'
-import CardStatVertical from '@components/card-statistics/Vertical'
-import Table from '@views/dashboard/Table'
-
 const DashboardAnalytics = () => {
+  const { data: session } = useSession()
+
   return (
     <div>
 
       <Grid container spacing={6}>
-      Halaman Dashboard Utama
+      <h1>Halaman Dashboard Utama</h1>
+      {session && (
+          <div>
+            <p>Nama : {session.user.name}</p>
+            <p>Email: {session.user.email}</p>
+            <p>Tipe Akun : {session.user.userType}</p>
+          </div>
+        )}
       </Grid>
     </div>
   )
