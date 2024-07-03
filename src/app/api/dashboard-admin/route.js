8e9f663218f3c69063b27838cd6c608150b769c0
Gesang Paudra J <g.paudra@gmail.com>
2024-07-03 09:29:05 +0700

    working API Dashboard admin

---
+++ src/app/api/dashboard-admin/route.js
@@ -0,0 +1,49 @@
+// API Dashboard Admin. Lokasi : /src/app/api/dashboard-admin/route.js
+
+import { NextResponse } from 'next/server'
+
+import prisma from '@/app/lib/prisma'
+
+export async function GET(req) {
+  try {
+    const kasbons = await prisma.kasbon.findMany({
+      select: {
+        id: true,
+        userId: true,
+        adminId: true,
+        jumlah: true,
+        status_r: true,
+        status_b: true,
+        keterangan: true,
+        metode: true,
+        createdAt: true,
+        updatedAt: true,
+        user: {
+          select: {
+            name: true, // Mengambil nama karyawan
+          },
+        },
+        admin: {
+          select: {
+            name: true, // Mengambil nama admin
+          },
+        },
+      },
+    })
+
+    // Konversi tanggal ke format ISO dan tambahkan nama karyawan dan admin
+    const formattedKasbons = kasbons.map(kasbon => ({
+      ...kasbon,
+      createdAt: kasbon.createdAt.toISOString(),
+      updatedAt: kasbon.updatedAt.toISOString(),
+      namaKaryawan: kasbon.user?.name || 'Tidak ada',
+      namaAdmin: kasbon.admin?.name || 'Tidak ada',
+    }))
+
+    return NextResponse.json(formattedKasbons, { status: 200 })
+  } catch (error) {
+    console.error('Error fetching kasbon data:', error)
+
+    return NextResponse.json({ error: 'Terjadi kesalahan saat mengambil data kasbon' }, { status: 500 })
+  }
+}
