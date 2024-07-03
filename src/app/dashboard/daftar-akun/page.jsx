e99e1e57e33ce11fe2f000d672bd153d1baa9e3d
Gesang Paudra J <g.paudra@gmail.com>
2024-07-03 18:46:40 +0700

    Revert "trying to make SSR components"
    This reverts commit 77e26d86e6946125709aabe35e2f61f5a6a0bc7c.

--- src/app/dashboard/daftar-akun/page.jsx
+++ src/app/dashboard/daftar-akun/page.jsx
@@ -1,55 +1,15 @@
-import React from 'react'
+import dynamic from 'next/dynamic'
 
-import { useSession } from 'next-auth/react'
+//Membuat komponen yang diimport menjadi dynamic page
+const TabelAkun = dynamic(()=>import('@views/tabel-akun/TabelAkun'),  {ssr:false})
 
-import TabelAkun from '@/views/tabel-akun/TabelAkun' // Impor komponen secara langsung
-
-
-
-export async function getServerSideProps(context) {
-
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const { data: session } = await useSession()
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/login',
-        permanent: false,
-      },
-    }
-  }
-
-  try {
-    const response = await fetch('/api/tabel-akun')
-    const data = await response.json()
-
-    return {
-      props: {
-        initialData: data,
-      },
-    }
-  } catch (error) {
-    console.error('Error fetching data:', error)
-
-    return {
-      notFound: true,
-    }
-  }
-}
-
-const DaftarAkun = ({ initialData }) => {
-  const { data: session } = useSession()
-
-  if (!session) {
-    return null // or redirect to login page
-  }
+const DaftarAkun = () => {
 
   return (
     <div style={{ height: 400, width: '80%' }}>
       <h1>Tabel Daftar Akun</h1>
       <br />
-      <TabelAkun initialData={initialData} />
+      <TabelAkun/>
     </div>
   )
 }
