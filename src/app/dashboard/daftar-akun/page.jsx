77e26d86e6946125709aabe35e2f61f5a6a0bc7c
Gesang Paudra J <g.paudra@gmail.com>
2024-07-03 18:35:57 +0700

    trying to make SSR components

--- src/app/dashboard/daftar-akun/page.jsx
+++ src/app/dashboard/daftar-akun/page.jsx
@@ -1,15 +1,55 @@
-import dynamic from 'next/dynamic'
+import React from 'react'
 
-//Membuat komponen yang diimport menjadi dynamic page
-const TabelAkun = dynamic(()=>import('@views/tabel-akun/TabelAkun'),  {ssr:false})
+import { useSession } from 'next-auth/react'
 
-const DaftarAkun = () => {
+import TabelAkun from '@/views/tabel-akun/TabelAkun' // Impor komponen secara langsung
+
+
+
+export async function getServerSideProps(context) {
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { data: session } = await useSession()
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    }
+  }
+
+  try {
+    const response = await fetch('/api/tabel-akun')
+    const data = await response.json()
+
+    return {
+      props: {
+        initialData: data,
+      },
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error)
+
+    return {
+      notFound: true,
+    }
+  }
+}
+
+const DaftarAkun = ({ initialData }) => {
+  const { data: session } = useSession()
+
+  if (!session) {
+    return null // or redirect to login page
+  }
 
   return (
     <div style={{ height: 400, width: '80%' }}>
       <h1>Tabel Daftar Akun</h1>
       <br />
-      <TabelAkun/>
+      <TabelAkun initialData={initialData} />
     </div>
   )
 }
