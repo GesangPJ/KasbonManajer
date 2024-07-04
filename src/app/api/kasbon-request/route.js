f91a09c7ba75110e59d1ec39e9cc5428f27a02a8
Gesang Paudra J <g.paudra@gmail.com>
2024-07-04 14:02:54 +0700

    working API get status request

--- src/app/api/kasbon-request/route.js
+++ src/app/api/kasbon-request/route.js
@@ -5,52 +5,52 @@ import { NextResponse } from "next/server"
 
 import prisma from "@/app/lib/prisma"
 
-export async function GET(req){
-  const {searchParams} = new URL(re.url)
+export async function GET(req) {
+  const { searchParams } = new URL(req.url)
   const userId = searchParams.get('userId')
   const statusR = "BELUM"
 
-  if(!userId){
-    return NextResponse.json({error:'User ID tidak ditemukan'}, {status : 400})
+  if (!userId) {
+    return NextResponse.json({ error: 'User ID tidak ditemukan' }, { status: 400 })
   }
 
-  console.log(userId)
+  console.log("User ID:", userId)
 
-  try{
+  try {
     const kasbons = await prisma.kasbon.findMany({
-      where:{
+      where: {
         status_r: statusR,
       },
-      select:{
-        id:true,
-        userId:true,
-        jumlah:true,
-        status_b:true,
-        keterangan:true,
-        metode:true,
-        updatedAt:true,
-        user:{
-          select:{
-            name:true,
-          }
-        }
+      select: {
+        id: true,
+        userId: true,
+        jumlah: true,
+        status_b: true,
+        keterangan: true,
+        metode: true,
+        updatedAt: true,
+        createdAt: true,
+        user: {
+          select: {
+            name: true,
+          },
+        },
       },
     })
 
-    const formattedKasbons = kasbons.map(kasbon=>({
+    const formattedKasbons = kasbons.map(kasbon => ({
       ...kasbon,
       createdAt: kasbon.createdAt.toISOString(),
       updatedAt: kasbon.updatedAt.toISOString(),
       namaKaryawan: kasbon.user?.name || '-',
     }))
 
-    return NextResponse.json(formattedKasbons, {status: 200})
+    console.log("Kasbons:", formattedKasbons)
+
+    return NextResponse.json(formattedKasbons, { status: 200 })
+  } catch (error) {
+    console.error("Error fetching kasbons:", error)
+
+    return NextResponse.json({ error: 'Ada kesalahan ketika mengembil data Kasbon' }, { status: 500 })
   }
-  catch (error){
-    return NextResponse.json({error:'Ada kesalahan ketika mengembil data Kasbon'}, {status:500})
-  }
-
-
-
-
 }
