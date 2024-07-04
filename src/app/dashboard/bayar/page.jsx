4a1f028544faf689da952e288274c780747773d6
Gesang Paudra J <g.paudra@gmail.com>
2024-07-04 21:39:39 +0700

    working status bayar page

--- src/app/dashboard/bayar/page.jsx
+++ src/app/dashboard/bayar/page.jsx
@@ -1,11 +1,13 @@
 
-
+import StatusBayar from "@/views/status-bayar/StatusBayar"
 
 
 const BayarKasbon = () =>{
   return(
     <div>
-      Halaman Bayar Kasbon
+      <h1>Halaman Status Bayar Kasbon</h1>
+      <br />
+      <StatusBayar/>
     </div>
   )
 }
