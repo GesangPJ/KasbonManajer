b6fe35d74d1072bb411717ff59bf0d5b0a786311
Gesang Paudra J <g.paudra@gmail.com>
2024-07-05 15:59:35 +0700

    add table admin

---
+++ src/views/tabel-akun/TabelAdmin.jsx
@@ -0,0 +1,70 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+
+import { useSession } from 'next-auth/react'
+import { DataGrid } from '@mui/x-data-grid'
+
+const columns = [
+  { field: 'no', headerName: 'No', width: 90 },
+  { field: 'name', headerName: 'Nama', width: 200 },
+  { field: 'email', headerName: 'Email', width: 200 },
+  { field: 'userType', headerName: 'Tipe Akun', width: 150 },
+]
+
+const TabelAkunAdmin = () => {
+  const { data: session } = useSession()
+  const [rows, setRows] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    if (session) {
+      const fetchData = async () => {
+        try {
+          const response = await fetch(`/api/tabel-akun?userId=${session.user.id}`)
+          const data = await response.json()
+          const filteredData = data.filter(row => row.userType === 'ADMIN')
+
+          // Tambahkan nomor urut
+          const numberedData = filteredData.map((row, index) => ({ ...row, no: index + 1 }))
+
+          setRows(numberedData)
+          setLoading(false)
+        } catch (error) {
+          console.error('Error fetching data:', error)
+        }
+      }
+
+      fetchData()
+    }
+  }, [session])
+
+  return (
+    <div style={{ height: 400, width: '80%' }}>
+      <h2 className='font-bold'>
+        Akun Admin
+      </h2>
+      <DataGrid
+        rows={rows}
+        sx={{
+          boxShadow: 2,
+          border: 2,
+          borderColor: 'primary.light',
+          '& .MuiDataGrid-cell:hover': {
+            color: 'primary.main',
+          },
+        }}
+        columns={columns}
+        pageSize={5}
+        pageSizeOptions={[5, 10, 25, 50, 100]}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+        loading={loading}
+        getRowId={(row) => row.id} // Tetap gunakan ID asli untuk identifikasi baris
+      />
+    </div>
+  )
+}
+
+export default TabelAkunAdmin
