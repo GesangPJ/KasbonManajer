d8a82a87dc40fbb4f2d646625ca2880b3013654d
Gesang Paudra J <g.paudra@gmail.com>
2024-07-05 08:04:16 +0700

    Update page.jsx

--- src/app/[...not-found]/page.jsx
+++ src/app/[...not-found]/page.jsx
@@ -3,18 +3,14 @@ import Providers from '@components/Providers'
 import BlankLayout from '@layouts/BlankLayout'
 import NotFound from '@views/NotFound'
 
-// Util Imports
-import { getServerMode } from '@core/utils/serverHelpers'
-
 const NotFoundPage = () => {
   // Vars
   const direction = 'ltr'
-  const mode = getServerMode()
 
   return (
     <Providers direction={direction}>
       <BlankLayout>
-        <NotFound mode={mode} />
+        <NotFound/>
       </BlankLayout>
     </Providers>
   )
