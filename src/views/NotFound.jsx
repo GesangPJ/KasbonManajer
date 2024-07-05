72627aa04c72ac26ea8db86c362c577c5f07cde4
Gesang Paudra J <g.paudra@gmail.com>
2024-07-05 08:09:45 +0700

    Update NotFound.jsx

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -7,25 +7,13 @@ import Link from 'next/link'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 
-// Component Imports
-import Illustrations from '@components/Illustrations'
-
-// Hook Imports
-import { useImageVariant } from '@core/hooks/useImageVariant'
-
-const NotFound = ({ mode }) => {
-  // Vars
-  const darkImg = '/images/pages/misc-mask-dark.png'
-  const lightImg = '/images/pages/misc-mask-light.png'
-
-  // Hooks
-  const miscBackground = useImageVariant(mode, lightImg, darkImg)
+const NotFound = () => {
 
   return (
     <div className='flex items-center justify-center min-bs-[100dvh] relative p-6 overflow-x-hidden'>
       <div className='flex items-center flex-col text-center gap-10'>
         <div className='flex flex-col gap-2 is-[90vw] sm:is-[unset]'>
-          <Typography className='font-medium text-8xl' color='error'>
+          <Typography className=' font-extrabold text-8xl' color='error'>
             404
           </Typography>
           <Typography variant='h4'>Page Not Found ⚠️</Typography>
