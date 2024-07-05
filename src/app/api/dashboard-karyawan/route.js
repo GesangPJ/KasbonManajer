9a5c7995190583506109a1e602f7267ac466d93c
Gesang Paudra J <g.paudra@gmail.com>
2024-07-05 13:44:45 +0700

    Add API Protection using JWT Token validation

--- src/app/api/dashboard-karyawan/route.js
+++ src/app/api/dashboard-karyawan/route.js
@@ -2,11 +2,21 @@
 
 import { NextResponse } from 'next/server'
 
+import { getToken } from 'next-auth/jwt'
+
 import prisma from '@/app/lib/prisma'
 
 export async function GET(req) {
   const { searchParams } = new URL(req.url)
   const userId = searchParams.get('userId')
+
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+
+  if (!token) {
+    console.log('Unauthorized Access : API Dashboard Karyawan')
+
+    return NextResponse.json({ error: 'Unauthorized Access' }, { status: 401 })
+  }
 
   if (!userId) {
     return NextResponse.json({ error: 'User ID tidak ditemukan' }, { status: 400 })
