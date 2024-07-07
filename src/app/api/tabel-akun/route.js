c7473180e9d9f67e63bad5c2c89b7f6cec825700
Gesang Paudra J <g.paudra@gmail.com>
2024-07-07 19:06:39 +0700

    fix API protection

--- src/app/api/tabel-akun/route.js
+++ src/app/api/tabel-akun/route.js
@@ -1,22 +1,24 @@
 import { NextResponse } from 'next/server'
 
-import { getToken } from 'next-auth/jwt'
+import { getServerSession } from "next-auth/next"
+
+import { authOptions } from "../auth/[...nextauth]/route"
 
 import prisma from '@/app/lib/prisma'
 
 
 // GET /api/tabel-akun
 export async function GET(req) {
-  const { searchParams } = new URL(req.url)
-  const userId = searchParams.get('userId')
+  const session = await getServerSession(req, { req }, authOptions)
 
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
-
-  if (!token) {
-    console.log('Unauthorized Access : API Tabel Akun')
+  if (!session) {
+    console.log('Unauthorized Access : API Ambil Daftar Akun')
 
     return NextResponse.json({ error: 'Unauthorized Access' }, { status: 401 })
   }
+
+  const { searchParams } = new URL(req.url)
+  const userId = searchParams.get('userId')
 
   if (!userId) {
     return NextResponse.json({ error: 'User ID tidak ditemukan' }, { status: 400 })
