4e4d674f4d266832db33d04cd1319bd0c951fae6
Gesang Paudra J <g.paudra@gmail.com>
2024-07-09 16:09:37 +0700

    Add page protection using Session Status

--- src/app/dashboard/cetak/page.jsx
+++ src/app/dashboard/cetak/page.jsx
@@ -1,8 +1,29 @@
+'use client'
 
+import { useEffect } from "react"
+
+import { useRouter } from "next/navigation"
+
+import { useSession } from "next-auth/react"
 
 import PrintLaporan from "@/views/print-laporan/PrintLaporan"
 
 const CetakKasbon = () =>{
+  const {data:session, status} = useSession()
+  const router = useRouter()
+
+  useEffect(()=>{
+    if (status === 'loading') return
+
+    if(!session){
+      router.push('/error/401')
+    }
+  }, [session, status, router])
+
+  if(!session){
+    return null
+  }
+
   return(
     <div>
       <div>
