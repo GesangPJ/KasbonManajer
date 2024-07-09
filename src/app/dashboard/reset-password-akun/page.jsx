4edb301ddf9fb6487b5a60b50742ec22601c857b
Gesang Paudra J <g.paudra@gmail.com>
2024-07-09 11:31:02 +0700

    working API to change karyawan password

--- src/app/dashboard/reset-password-akun/page.jsx
+++ src/app/dashboard/reset-password-akun/page.jsx
@@ -1,10 +1,37 @@
+'use client'
 
+import { useEffect } from 'react'
+
+import { useRouter } from 'next/navigation'
+
+import { useSession } from 'next-auth/react'
+
+import ResetPasswordKaryawan from "@/views/reset-password/resetpassword-karyawan"
 
 const ResetPasswordAkun = () =>{
+  const { data: session, status } = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (status === 'loading') return // Jangan lakukan apa pun saat sesi sedang dimuat
+
+    if (!session) {
+      router.push('/error/401')
+    }
+  }, [session, status, router])
+
+  if (!session) {
+    return null
+  }
+
   return(
     <div>
-      Halaman Reset Password Akun
-
+      <h1>Reset Password Akun</h1>
+      <br />
+      <div>
+        <ResetPasswordKaryawan/>
+      </div>
+      <br />
     </div>
   )
 }
